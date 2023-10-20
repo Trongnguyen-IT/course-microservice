0b2a4d86e82b64225fac9309b3fa4420179d877c
@Trongnguyen-IT <nguyennhattrong@gmail.com>
2023-10-20 16:09:10 +0700

    Basket API

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart cart);
+        Task DeleteBasket(string userName);
+    }
+
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            _redisCache = distributedCache;
+        }
+
+        public async Task<ShoppingCart?> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if(string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject <ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            await _redisCache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+            return await GetBasket(cart.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
